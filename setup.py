b12fa7f50fe663a55967aa0991c8e8ddb4b74928
mhsiddiqui <mhassan.eeng@gmail.com>
2024-10-18 11:19:05 +0500

    Poetry settings updated

--- setup.py
+++ setup.py
@@ -16,5 +16,14 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Operating System :: OS Independent",
+        "Framework :: FastAPI",
+        "Typing :: Typed",
     ],
 )
